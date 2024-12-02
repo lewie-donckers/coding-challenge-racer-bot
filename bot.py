934bba1c656369c6e068a8ed96b5c459af3b66f0
Lewie Donckers <l.donckers@xs4all.nl>
2024-12-02 23:41:27 +0100

    Another update

--- bot.py
+++ bot.py
@@ -18,36 +18,24 @@ from ...linear_math import Transform
 # - max throttle ac-/deceleration = 100 pixels/sec2
 # - sideways (drifting) deceleration = 200 pixels/sec2
 
-DEBUG = True
+DEBUG = False
 DRAW_FLAME = True
 
 # TUNING PARAMETERS
-EFFECTIVE_DECELERATION = -155  # pixels/sec2
-STEER_DISTANCE_LIMIT = 42  # pixels
-STEER_ANGLE_LIMIT = 42  # degrees
-SPEED_LIMIT_FACTOR = 105  # pixels/sec ratio
-CORNER_CUT_FACTOR = 50  # percent
-CORNER_CUT_DISTANCE = 0  # pixels
-STEERING_FACTOR = 60  # percent
+EFFECTIVE_DECELERATION = -108  # pixels/sec2
+STEER_DISTANCE_LIMIT = 49  # pixels
+STEER_ANGLE_LIMIT = 36  # degrees
+SPEED_LIMIT_FACTOR = 67  # pixels/sec ratio
+SPEED_LIMIT_OFFSET = 90  # pixels/sec
+CORNER_CUT_FACTOR = 53  # percent
+STEERING_FACTOR = 55  # percent
 
-# TRACK1: 90.817 (WITH SPINOUT)
-# TRACK2: 78.100
+# TRACK1: 84.517
+# TRACK2: 75.700
 
 # TIMES TO BEAT
 # TRACK1: 97.150 - CaravanRacer
 # TRACK2: 93.450 - Schummi
-
-
-def lerp(min, max, factor):
-    return (1 - factor) * min + factor * max
-
-
-def inv_lerp(min, max, val):
-    return (val - min) / (max - min)
-
-
-def remap(min1, max1, min2, max2, val1):
-    return lerp(min2, max2, inv_lerp(min1, max1, val1))
 
 
 def sign(angle):
@@ -154,8 +142,8 @@ class Gonzales(Bot):
                  steer_distance_limit=STEER_DISTANCE_LIMIT,
                  steer_angle_limit=STEER_ANGLE_LIMIT,
                  speed_limit_factor=SPEED_LIMIT_FACTOR,
+                 speed_limit_offset=SPEED_LIMIT_OFFSET,
                  corner_cut_factor=CORNER_CUT_FACTOR,
-                 corner_cut_distance=CORNER_CUT_DISTANCE,
                  steering_factor=STEERING_FACTOR):
         super().__init__(track)
 
@@ -163,16 +151,16 @@ class Gonzales(Bot):
         self._steer_distance_limit = steer_distance_limit
         self._steer_angle_limit = steer_angle_limit
         self._speed_limit_factor = speed_limit_factor
+        self._speed_limit_offset = speed_limit_offset
         self._corner_cut_factor = corner_cut_factor
-        self._corner_cut_distance = corner_cut_distance
         self._steering_factor = steering_factor
 
         self._original_track = Track(self.track.lines)
         self._track = self._optimizeTrack()
 
         self._speed_limits = [
-            self._speed_limit_factor * p.dist_closest() / abs(p.angle())
-            for p in self._track
+            (self._speed_limit_factor * p.dist_closest() / abs(p.angle())) +
+            self._speed_limit_offset for p in self._track
         ]
 
         if DRAW_FLAME:
@@ -194,24 +182,10 @@ class Gonzales(Bot):
         return "Lewie"
 
     def _optimizeTrack(self):
-        ccf = []
-        for p in self._original_track:
-            should_cut = False
-            if p.is_apex():
-                should_cut = True
-            else:
-                next_apex = p.next_apex()
-                prev_apex = p.prev_apex()
-
-                # should_cut = (sign(next_apex.angle()) == sign(
-                #     prev_apex.angle())) and (p.dist_to(next_apex.index) < 200
-                #                              or
-                #                              prev_apex.dist_to(p.index) < 200)
-                should_cut = (
-                    p.dist_to(next_apex.index) < self._corner_cut_distance
-                    or prev_apex.dist_to(p.index) < self._corner_cut_distance)
-
-            ccf.append(self._corner_cut_factor if should_cut else 0)
+        ccf = [
+            self._corner_cut_factor if p.is_apex() else 0
+            for p in self._original_track
+        ]
         ccf = [
             c if c > 0 else max(p, n) / 2
             for p, c, n in sliding_window(ccf[-1:] + ccf + ccf[:1], 3)
@@ -231,26 +205,28 @@ class Gonzales(Bot):
         return Track(result)
 
     def _goFast(self, wp, pos, velocity):
+        self._last_brake_wp = None  # debug
+
         target = self._track.get_item_wrap(wp)
 
         throttle = 1
 
         v = velocity.length()
-        mt = -v / self._effective_deceleration
-        md = 0.5 * self._effective_deceleration * mt * mt + v * mt
-
+        mt = v / -self._effective_deceleration
+        md = mt * v / 2
         d = (pos.p - target.point).length()
-        self._last_brake_wp = None  # debug
+
         while md > d:
             tv = self._speed_limits[target.index]
-            ts = (tv - v) / self._effective_deceleration
-            td = 0.5 * self._effective_deceleration * ts * ts + v * ts
-            if td > d:
-                throttle = -1
-                self._last_brake_wp = target.index  # debug
-                break
-            target = target.nxt()
+            if tv < v:
+                ts = (v - tv) / -self._effective_deceleration
+                td = ts * (tv + v) / 2
+                if td > d:
+                    throttle = -1
+                    self._last_brake_wp = target.index  # debug
+                    break
             d += target.dist_to_nxt()
+            target = target.nxt()
 
         return throttle
 
@@ -266,7 +242,7 @@ class Gonzales(Bot):
             target = target.nxt()
             angle = (posinverse * target.point).as_polar()[1]
 
-        if DEBUG: self._last_steer_wp = wp
+        if DEBUG: self._last_steer_wp = target.index
 
         return angle * (self._steering_factor / 100)
 
@@ -297,13 +273,13 @@ class Gonzales(Bot):
         #                  2)
 
         # SPEED LIMITS
-        # for i in range(len(self.track.lines)):
-        #     p = self.track.lines[i] * zoom
-        #     m = self._speed_limits[i]
-        #     text = self._font.render(
-        #         f'{m:.2f}', True, self._red
-        #         if self._original_track[i].is_apex() else self._black)
-        #     map_scaled.blit(text, p)
+        for i in range(len(self.track.lines)):
+            p = self.track.lines[i] * zoom
+            m = self._speed_limits[i]
+            text = self._font.render(
+                f'{m:.2f}', True, self._red
+                if self._original_track[i].is_apex() else self._black)
+            map_scaled.blit(text, p)
 
         # STEER WP
         pygame.draw.line(
---
+++ presentation/slides.md
@@ -0,0 +1,85 @@
+---
+title: Gonzales
+subtitle: Brawn over brains
+---
+
+###
+
+<img class="r-stretch" src="images/speedy.png" width=50% />
+
+### Timeline
+
+![](images/calendar_1.png)
+
+###
+
+![](images/calendar_2.png)
+
+###
+
+![](images/calendar_3.png)
+
+###
+
+![](images/calendar_4.png)
+
+### High level
+
+- at startup: analyse track
+- every tick:
+    - `self._goFast()`
+    - `self._thatWay()`
+
+### Track analysis
+
+- determine apexes
+- cut corners in apexes
+    - by `CORNER_CUT_FACTOR`
+- cut corners in adjacent waypoints
+    - by half of that
+
+### Track analysis
+
+- determine speed limits
+    - waypoint sharpness = distance to closest neighbour / angle
+    - `SPEED_LIMIT_FACTOR` * sharpness + `SPEED_LIMIT_OFFSET`
+
+### Track analysis
+
+<img src="images/analysis.png" width=75%/>
+
+### Going fast
+
+- determine distance to stop
+    - using `EFFECTIVE_DECELERATION`
+- check speed limit of waypoints in that distance
+    - determine distance to brake for that limit
+    - if so, brake!
+- else, go fast!
+
+### Going in the right direction
+
+- check if the next waypoint is 'guaranteed'
+    - using `STEER_DISTANCE_LIMIT` and `STEER_ANGLE_LIMIT`
+    - if so, start steering to waypoint after that
+- calculate steering output based on angle
+    - using `STEERING_FACTOR`
+
+### Racing
+
+<img src="images/racing.png" width=75%/>
+
+### Parameters
+
+- `CORNER_CUT_FACTOR`
+- `SPEED_LIMIT_FACTOR`
+- `SPEED_LIMIT_OFFSET`
+- `EFFECTIVE_DECELERATION`
+- `STEER_DISTANCE_LIMIT`
+- `STEER_ANGLE_LIMIT`
+- `STEERING_FACTOR`
+
+### Brawn over brains
+
+TODO brute_force.py
+
